5af3a751daec8410bc622c55d3dfd3235ace87a6
Jacob Frick <jacobhfrick1992@gmail.com>
2020-03-24 13:00:21 -0700

    added routes

--- public/src/App.js
+++ public/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import CreateBlog from './components/CreateBlog'
 function App() {
   return (
-    <h1>Blog App</h1>
+    <Router>
+      <Switch>
+        <Route path="/profile">
+          <h1>Profile</h1>
+        </Route>
+        <Route path="/createBlog">
+          <CreateBlog />
+        </Route>
+        <Route path="/">
+          <h1>Home</h1>
+        </Route>
+      </Switch>
+  </Router>
   )
 }
 
